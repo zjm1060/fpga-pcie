9a05235134a62d8effc5d8b054583aa588815fb2
zjm <zjm1060@163.com>
2022-11-11 03:25:01 +0000

    buffer used

--- BBN_FPGA.c
+++ BBN_FPGA.c
@@ -1,10 +1,6 @@
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/pci.h>
-#include <linux/fs.h>
-#include <linux/cdev.h>
-#include <linux/uaccess.h>
-#include <linux/sched.h>
+#include "BBN_FPGA.h"
+
+// #include "buffer.h"
 
 MODULE_LICENSE("GPL");
 
@@ -14,6 +10,8 @@ MODULE_LICENSE("GPL");
 #define DRIVER_NAME "bbn_fpga"
 #define BOARD_NAME "piecomm1"
 
+#define BUFFER_SIZE (32*1024)
+
 struct DevInfo_t {
     struct pci_dev *pciDev;
 
@@ -21,6 +19,17 @@ struct DevInfo_t {
     dev_t cdevNum;
     struct cdev cdev;
     struct class *cls;
+
+    /* Mutex for this device. */
+    struct semaphore rx_sem;
+    wait_queue_head_t rxq; 
+
+    struct kfifo rx_fifo;
+
+    void *buffer;
+
+    uint32_t interrupt_count;
+    uint32_t read_count;
 
     /* kernel's virtual addr. for the mapped BARs */
     void __iomem *ptr_bar0;
@@ -33,11 +42,70 @@ static struct pci_device_id idTable[] = {
 
 MODULE_DEVICE_TABLE(pci, idTable);
 
+int fpga_open(struct inode *inode, struct file *filePtr) 
+{
+    struct DevInfo_t *devInfo = NULL;
+
+    devInfo = container_of(inode->i_cdev, struct DevInfo_t, cdev);
+
+    // if (down_interruptible(&devInfo->sem)) {
+	// 	printk(KERN_WARNING "[BBN FPGA] fpga_open: Unable to get semaphore!\n");
+	// 	return -ERESTARTSYS;
+	// }
+
+    filePtr->private_data = devInfo;
+
+    // up(&devInfo->sem);
+
+    return 0;
+}
+
+ssize_t fpga_read(struct file *filePtr, char __user *buf, size_t count, loff_t *pos)
+{
+    struct DevInfo_t * devInfo = (struct DevInfo_t *) filePtr->private_data;
+    ssize_t bytesDone = 0;
+
+    if (down_interruptible(&devInfo->rx_sem)) {
+		printk(KERN_WARNING "[BBN FPGA] fpga_open: Unable to get semaphore!\n");
+		return -ERESTARTSYS;
+	}
+
+    // while(kfifo_len(&devInfo->rx_fifo) < count){
+    //     up(&devInfo->rx_sem);
+    //     if(wait_event_interruptible(devInfo->rxq, kfifo_len(&devInfo->rx_fifo) >= count)){
+    //         return -ERESTARTSYS;
+    //     }
+    //     if(down_interruptible(&devInfo->rx_sem)){
+    //         return -ERESTARTSYS;
+    //     }
+    // }
+
+    while(devInfo->read_count == devInfo->interrupt_count){
+        up(&devInfo->rx_sem);
+        if(wait_event_interruptible(devInfo->rxq, devInfo->read_count != devInfo->interrupt_count)){
+            return -ERESTARTSYS;
+        }
+        if(down_interruptible(&devInfo->rx_sem)){
+            return -ERESTARTSYS;
+        }
+    }
+
+    memcpy_fromio(devInfo->buffer, devInfo->ptr_bar0, BUFFER_SIZE);
+
+    bytesDone = copy_to_user(buf, devInfo->buffer, BUFFER_SIZE);
+
+    devInfo->read_count ++;
+
+    up(&devInfo->rx_sem);
+
+    return bytesDone;
+}
+
 struct file_operations fileOps = {
 	.owner =    THIS_MODULE,
-	// .read =     fpga_read,
+	.read =     fpga_read,
 	// .write =    fpga_write,
-	// .open =     fpga_open,
+	.open =     fpga_open,
 	// .release =  fpga_close,
 };
 
@@ -80,8 +148,12 @@ static int setup_chrdev(struct DevInfo_t *devInfo){
 
 static irqreturn_t ms_pci_interrupt(int irq, void* dev_id)
 {
-    // struct pci_dev *dev = dev_id;
-    // struct DevInfo_t *devInfo = pci_get_drvdata(dev);
+    struct pci_dev *dev = dev_id;
+    struct DevInfo_t *devInfo = pci_get_drvdata(dev);
+
+    devInfo->interrupt_count ++;
+
+    wake_up_interruptible(&devInfo->rxq);
 
     return IRQ_HANDLED;
 }
@@ -136,6 +208,13 @@ static int probe(struct pci_dev *dev, const struct pci_device_id *id)
 	}
     printk("[BBN FPGA] - nvec = %d\n",status);
 	printk("[BBN FPGA] - request irq:%d\n", dev->irq);
+
+    status = kfifo_alloc(&devInfo->rx_fifo, 1024, GFP_KERNEL);
+    devInfo->buffer = kmalloc (BUFFER_SIZE * sizeof(char), GFP_KERNEL);
+
+    init_waitqueue_head(&devInfo->rxq);
+
+    sema_init(&devInfo->rx_sem, 1);
 
     status = request_irq(pci_irq_vector(dev, 0), ms_pci_interrupt, IRQF_SHARED, DRIVER_NAME, dev);
     if(status){
