93b4945f3fbf5cf4e2046cc9334df1fe5af43baa
zjm098 <zjm1060@163.com>
2022-11-11 11:34:30 +0800

    add BBN_FPGA.h back

---
+++ BBN_FPGA.h
@@ -0,0 +1,15 @@
+#ifndef __BBN_FPGA_H
+#define __BBN_FPGA_H
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/pci.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
+#include <linux/sched.h>
+#include <linux/kfifo.h>
+
+struct DevInfo_t;
+
+#endif
